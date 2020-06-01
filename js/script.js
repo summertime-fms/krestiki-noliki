e2cfe792fd15460a18ac149b1cc6c33630385620
summertime-fms <danyushin0107@gmail.com>
2020-06-01 16:05:44 +0300

    исправляет логику текстового сообщения о первом ходе

--- js/script.js
+++ js/script.js
@@ -5,9 +5,30 @@ for(let i=0; i < 9; i++) {
 let allBlocks = document.getElementsByClassName("block");
 let fieldMain = document.querySelector(".field");
 let hod = 0;
-let zeroFirst = Math.round(Math.random());
 let playButton = document.querySelector(".field__button--play");
 let restartButton = document.querySelector(".field__button--restart");
+
+
+function whoFirst() {
+    let zeroFirst = Math.round(Math.random());
+
+    if (zeroFirst) {
+        let message = document.createElement("p");
+        message.className = 'field__info';
+        message.innerHTML = 'Первыми ходят крестики!';
+    
+        fieldMain.append(message);
+        hod = zeroFirst;
+        
+    } else {
+        let message = document.createElement("p");
+        message.className = 'field__info';
+        message.innerHTML = 'Первыми ходят нолики!';
+    
+        fieldMain.append(message);
+        hod = zeroFirst; 
+    }
+    }
 
 
 playButton.addEventListener("click", function() {
@@ -15,32 +36,19 @@ playButton.addEventListener("click", function() {
     allBlocks[i].style.cursor = "pointer";
     }
 
-if (zeroFirst) {
-    let message = document.createElement("p");
-    message.className = 'field__info';
-    message.innerHTML = 'Первыми ходят крестики!';
-
-    fieldMain.append(message);
-    hod = zeroFirst;
-    
-} else {
-    let message = document.createElement("p");
-    message.className = 'field__info';
-    message.innerHTML = 'Первыми ходят нолики!';
-
-    fieldMain.append(message);
-    hod = zeroFirst; 
-}
-
-
-
+   whoFirst();
 
 let field = document.querySelector(".field__wrapper");
+
 field.onclick = function(event) {
+
     if (event.target.className == 'block') {
+
         if (hod % 2 == 0) {
+
             event.target.classList.add("zero");
         } else {
+
             event.target.classList.add("cross");
         }
         
@@ -48,27 +56,30 @@ field.onclick = function(event) {
         checkWinner();
     }
 } });
-    function checkWinner() {
-        let allBlocks = document.getElementsByClassName("block");
-        let winComb = [
-            [allBlocks[0], allBlocks[1], allBlocks[2]],
-            [allBlocks[3], allBlocks[4], allBlocks[5]],
-            [allBlocks[6], allBlocks[7], allBlocks[8]],
-            [allBlocks[0], allBlocks[3], allBlocks[6]],
-            [allBlocks[1], allBlocks[4], allBlocks[7]],
-            [allBlocks[2], allBlocks[5], allBlocks[8]],
-            [allBlocks[0], allBlocks[4], allBlocks[8]],
-            [allBlocks[2], allBlocks[4], allBlocks[6]]
-        ];
-        for (let i = 0; i < winComb.length; i++) {
-            if (winComb[i][0].classList.contains('cross') && winComb[i][1].classList.contains("cross") && winComb[i][2].classList.contains("cross")) {
+
+function checkWinner() {
+    let allBlocks = document.getElementsByClassName("block");
+    let winComb = [
+        [allBlocks[0], allBlocks[1], allBlocks[2]],
+        [allBlocks[3], allBlocks[4], allBlocks[5]],
+        [allBlocks[6], allBlocks[7], allBlocks[8]],
+        [allBlocks[0], allBlocks[3], allBlocks[6]],
+        [allBlocks[1], allBlocks[4], allBlocks[7]],
+        [allBlocks[2], allBlocks[5], allBlocks[8]],
+        [allBlocks[0], allBlocks[4], allBlocks[8]],
+        [allBlocks[2], allBlocks[4], allBlocks[6]]
+    ];
+    for (let i = 0; i < winComb.length; i++) {
+
+        if (winComb[i][0].classList.contains('cross') && winComb[i][1].classList.contains("cross") && winComb[i][2].classList.contains("cross")) {
+
                 highlight(winComb[i])
                 restartButton.classList.add('button--showed');
                 playButton.setAttribute('disabled', '')
-
-               
+ 
                 break
             } else if (winComb[i][0].classList.contains('zero') && winComb[i][1].classList.contains("zero") && winComb[i][2].classList.contains("zero")) {
+
                 highlight(winComb[i])
                 restartButton.classList.add('button--showed');
                 playButton.setAttribute('disabled', '')
@@ -77,22 +88,25 @@ field.onclick = function(event) {
             }
         
     }
-    (document.querySelector(".field__button--restart")).addEventListener("click", function() {
-        reboot();
-    })
-    function reboot() {
-        for (let i = 0; i < allBlocks.length; i++) {
-            allBlocks[i].classList.remove("zero");
-            allBlocks[i].classList.remove("cross");
-            allBlocks[i].classList.remove("block--red");
-            allBlocks[i].classList.remove("block--disable");
-        }
-        document.querySelector('.field__info').textContent = '';
-        hod = 0;
-    }
 
     
 }
+function reboot() {
+    for (let i = 0; i < allBlocks.length; i++) {
+        allBlocks[i].classList.remove("zero");
+        allBlocks[i].classList.remove("cross");
+        allBlocks[i].classList.remove("block--red");
+        allBlocks[i].classList.remove("block--disable");
+    }
+    
+    hod = 0;
+}
+
+restartButton.addEventListener("click", function() {
+    reboot();
+    document.querySelector('.field__info').remove();
+    whoFirst();
+})
 
 
 function highlight(element) {
